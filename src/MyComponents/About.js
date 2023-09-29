294034fb55afadca0762f443314f048f32e5d9be
Atharvk-512 <atharvk.2019@gmail.com>
2023-09-29 11:22:48 +0530

    completed app

---
+++ src/MyComponents/About.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export const About = () => {
+  let myStyle = {
+    minHeight: "75vh",
+    margin: "50px auto",
+  };
+  return (
+    <>
+      <div style={myStyle}>
+        <h3>
+            This is About component
+        </h3>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam
+          ducimus, voluptas atque officia libero deserunt. Dignissimos dolores
+          soluta corporis quis odit cupiditate nemo temporibus quam nulla vel,
+          fugiat animi veniam consequuntur non culpa sed. Lorem, ipsum dolor sit
+          amet consectetur adipisicing elit. Soluta obcaecati numquam sunt hic
+          illo animi odio alias officia impedit quas rem ullam delectus dolore
+          ea ipsum a, iste quidem? Porro sapiente, asperiores pariatur iure
+          repellendus libero rem, ab enim non explicabo quibusdam nulla sunt
+          minus quisquam voluptatum aspernatur ut. Vitae?
+        </p>
+      </div>
+    </>
+  );
+};
